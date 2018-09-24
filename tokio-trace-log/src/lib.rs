f272050b04bd436281152b0fb8d11532440a39e0
Eliza Weisman <eliza@buoyant.io>
2018-09-24 15:04:41 -0700

    first pass on an adapter for the log crate

---
+++ tokio-trace-log/src/lib.rs
@@ -0,0 +1,31 @@
+extern crate tokio_trace;
+extern crate log;
+extern crate env_logger;
+
+use std::{io, time::Instant};
+use tokio_trace::subscriber::Subscriber;
+
+pub fn try_init() -> Result<(), log::SetLoggerError> {
+    env_logger::Builder::from_default_env()
+        .format(|_, record| format_trace(record))
+        .try_init()
+}
+
+pub fn init() {
+    try_init().unwrap()
+}
+
+pub fn format_trace(record: &log::Record) -> io::Result<()> {
+    let parent = tokio_trace::Span::current();
+    let meta: tokio_trace::Meta = record.into();
+    let event = tokio_trace::Event {
+        timestamp: Instant::now(),
+        parent,
+        follows_from: &[],
+        meta: &meta,
+        field_values: &[],
+        message: record.args().clone()
+    };
+    tokio_trace::Dispatcher::current().observe_event(&event);
+    Ok(())
+}
