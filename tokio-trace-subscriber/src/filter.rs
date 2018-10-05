d2fda132c46e4bd34c1780c3741aa58b9baa79fc
Eliza Weisman <eliza@buoyant.io>
2018-10-05 14:06:34 -0700

    Sketch sampling filter

--- tokio-trace-subscriber/src/filter.rs
+++ tokio-trace-subscriber/src/filter.rs
@@ -1,6 +1,8 @@
 use super::Filter;
 use tokio_trace::Meta;
 
+use std::sync::atomic::{Ordering, AtomicUsize};
+
 pub trait FilterExt: Filter {
 
     /// Construct a new `Filter` that enables a span or event if both `self`
@@ -42,6 +44,13 @@ pub struct Or<A, B> {
     b: B,
 }
 
+/// A filter that enables some fraction of events.
+#[derive(Debug)]
+pub struct Sample {
+    every: usize,
+    count: AtomicUsize,
+}
+
 impl<A, B> Filter for And<A, B>
 where
     A: Filter,
@@ -73,6 +82,38 @@ where
     fn should_invalidate_filter(&self, metadata: &Meta) -> bool {
         self.a.should_invalidate_filter(metadata) ||
         self.b.should_invalidate_filter(metadata)
+    }
+}
+
+impl Sample {
+    /// Construct a new filter that is enabled for every `every` spans/events.
+    pub fn every(every: usize) -> Self {
+        Self {
+            every,
+            count: AtomicUsize::new(0),
+        }
+    }
+
+    // TODO: constructors with ratios, percentages, etc?
+}
+
+impl Filter for Sample {
+    fn enabled(&self, metadata: &Meta) -> bool {
+        // TODO: it would be nice to be able to have a definition of sampling
+        // that also enables all the children of a sampled span...figure that out.
+        let current = self.count.fetch_add(1, Ordering::Acquire);
+        if current % self.every == 0 {
+            self.count.store(0, Ordering::Release);
+            true
+        } else {
+            false
+        }
+    }
+
+    fn should_invalidate_filter(&self, metadata: &Meta) -> bool {
+        // The filter _needs_ to be re-evaluated every time, or else the counter
+        // won't be updated.
+        true
     }
 }
 
