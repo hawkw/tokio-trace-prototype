5cf514d8b33f684fe9e2608b7f9a852ec163daa0
Eliza Weisman <eliza@buoyant.io>
2018-10-08 14:32:23 -0700

    Add docs and examples to observer combinators

--- tokio-trace-subscriber/src/observe.rs
+++ tokio-trace-subscriber/src/observe.rs
@@ -2,7 +2,41 @@ use ::{Observe, Filter};
 use tokio_trace::{Event, SpanData, Meta};
 
 pub trait ObserveExt: Observe {
-    /// Construct a new observer that sends events to both `self` and `other.
+    /// Construct a new observer that sends events to both `self` and `other`.
+    ///
+    /// For example:
+    /// ```
+    /// #[macro_use]
+    /// extern crate tokio_trace;
+    /// extern crate tokio_trace_subscriber;
+    /// use tokio_trace_subscriber::{registry, filter, Observe, ObserveExt};
+    /// # use tokio_trace::{Level, Meta};
+    /// # fn main() {
+    ///
+    /// let observer = tokio_trace_log::TraceLogger::new()
+    ///     // Subscribe *only* to spans named "foo".
+    ///     .with_filter(|meta: &Meta| {
+    ///         meta.name == Some("foo")
+    ///     });
+    ///
+    /// let subscriber = tokio_trace_subscriber::Composed::builder()
+    ///     .with_observer(observer)
+    ///     .with_registry(registry::increasing_counter);
+    ///
+    /// tokio_trace::Dispatch::to(subscriber).with(|| {
+    ///     /// // This span will be logged.
+    ///     span!("foo", enabled = true) .enter(|| {
+    ///         // do work;
+    ///     });
+    ///     // This span will *not* be logged.
+    ///     span!("bar", enabled = false).enter(|| {
+    ///         // This event also will not be logged.
+    ///         event!(Level::Debug, { enabled = false },"this won't be logged");
+    ///     });
+    /// });
+    /// # }
+    /// ```
+    ///
     fn tee_to<I>(self, other: I) -> Tee<Self, I::Observer>
     where
         I: IntoObserver,
@@ -20,6 +54,58 @@ pub trait ObserveExt: Observe {
     /// external crates with user-defined filters, so that the resulting
     /// observer is [`enabled`] only for a subset of the events and spans for
     /// which the original observer would be enabled.
+    ///
+    /// For example:
+    /// ```
+    /// #[macro_use]
+    /// extern crate tokio_trace;
+    /// extern crate tokio_trace_subscriber;
+    /// use tokio_trace_subscriber::{registry, Observe, ObserveExt};
+    /// # use tokio_trace_subscriber::{Filter, filter::NoFilter};
+    /// # use tokio_trace::{Level, Meta, Event, SpanData};
+    /// # fn main() {
+    ///
+    /// struct Foo {
+    ///     // ...
+    /// }
+    ///
+    /// struct Bar {
+    ///     // ...
+    /// }
+    ///
+    /// impl Observe for Foo {
+    ///     // ...
+    /// # fn observe_event<'event, 'meta: 'event>(&self, _: &'event Event<'event, 'meta>) {}
+    /// # fn enter(&self, _: &SpanData) {}
+    /// # fn exit(&self, _: &SpanData) {}
+    /// # fn filter(&self) -> &dyn Filter { &NoFilter}
+    /// }
+    ///
+    /// impl Observe for Bar {
+    ///     // ...
+    /// # fn observe_event<'event, 'meta: 'event>(&self, _: &'event Event<'event, 'meta>) {}
+    /// # fn enter(&self, _: &SpanData) {}
+    /// # fn exit(&self, _: &SpanData) {}
+    /// # fn filter(&self) -> &dyn Filter { &NoFilter}
+    /// }
+    ///
+    /// let foo = Foo { };
+    /// let bar = Bar { };
+    ///
+    /// let observer = foo.tee_to(bar);
+    ///
+    /// let subscriber = tokio_trace_subscriber::Composed::builder()
+    ///     .with_observer(observer)
+    ///     .with_registry(registry::increasing_counter);
+    ///
+    /// tokio_trace::Dispatch::to(subscriber).with(|| {
+    ///     // This span will be seen by both `foo` and `bar`.
+    ///     span!("my great span").enter(|| {
+    ///         // ...
+    ///     })
+    /// });
+    /// # }
+    /// ```
     ///
     /// [`Filter`]: ../trait.Filter.html
     /// [`enabled`]: ../trait.Filter.html#tymethod.enabled
@@ -40,20 +126,80 @@ pub trait IntoObserver {
     fn into_observer(self) -> Self::Observer;
 }
 
+/// An observer which does nothing.
 pub struct NoObserver;
 
+/// An observer which is an instance of one of two types that implement
+/// `Observe`.
+///
+/// This is intended to be used when an observer implementation is chosen
+/// conditionally, and the overhead of `Box<dyn Observe>` is unwanted.
+///
+/// For example:
+/// ```
+/// # extern crate tokio_trace;
+/// extern crate tokio_trace_subscriber;
+/// use tokio_trace_subscriber::{observe, Observe};
+/// # use tokio_trace_subscriber::{Filter, filter::NoFilter};
+/// # use tokio_trace::{Event, SpanData};
+/// # fn main() {}
+///
+/// struct Foo {
+///     // ...
+/// }
+///
+/// struct Bar {
+///     // ...
+/// }
+///
+/// impl Observe for Foo {
+///     // ...
+/// # fn observe_event<'event, 'meta: 'event>(&self, _: &'event Event<'event, 'meta>) {}
+/// # fn enter(&self, _: &SpanData) {}
+/// # fn exit(&self, _: &SpanData) {}
+/// # fn filter(&self) -> &dyn Filter { &NoFilter}
+/// }
+///
+/// impl Observe for Bar {
+///     // ...
+/// # fn observe_event<'event, 'meta: 'event>(&self, _: &'event Event<'event, 'meta>) {}
+/// # fn enter(&self, _: &SpanData) {}
+/// # fn exit(&self, _: &SpanData) {}
+/// # fn filter(&self) -> &dyn Filter { &NoFilter}
+/// }
+///
+/// fn foo_or_bar(foo: bool) -> observe::Either<Foo, Bar> {
+///     if foo {
+///         observe::Either::A(Foo { })
+///     } else {
+///         observe::Either::B(Bar { })
+///     }
+/// }
+/// ```
 #[derive(Copy, Clone)]
 pub enum Either<A, B> {
     A(A),
     B(B),
 }
 
+/// An observer that forwards events and spans to two other types implementing
+/// `Observe`.
+///
+/// The `Tee`'s filter composes the filters of its child observers, so that a
+/// span or event is enabled if either of the child observers' filters consider
+/// it enabled. Similarly, cached filter evaluations should be invalidated if
+/// either child observer's filter indicates that they should be.
 #[derive(Copy, Clone)]
 pub struct Tee<A, B> {
     a: A,
     b: B,
 }
 
+/// An observer composed with an additional filter.
+///
+/// This observer's filter considers a span or event enabled if **both** the
+/// wrapped observer's filter and the composed filter enable it. However, cached
+/// filters are invalidated if **either** filter indicates that they should be.
 #[derive(Debug, Clone)]
 pub struct WithFilter<O, F> {
     inner: O,
