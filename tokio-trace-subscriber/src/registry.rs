c7cd57681d69a4ff4ae0d2290b2e2e82d570b6bc
Eliza Weisman <eliza@buoyant.io>
2018-10-08 10:47:15 -0700

    Fix warnings

--- tokio-trace-subscriber/src/registry.rs
+++ tokio-trace-subscriber/src/registry.rs
@@ -5,7 +5,7 @@ use std::sync::atomic::{AtomicUsize, ATOMIC_USIZE_INIT, Ordering};
 /// Registers new span IDs with an increasing `usize` counter.
 ///
 /// This should not be used on 32-bit machines.
-pub fn increasing_counter(new_span: &NewSpan) -> Id {
+pub fn increasing_counter(_new_span: &NewSpan) -> Id {
     static NEXT_ID: AtomicUsize = ATOMIC_USIZE_INIT;
     let next = NEXT_ID.fetch_add(1, Ordering::SeqCst);
     Id::from_u64(next as u64)
