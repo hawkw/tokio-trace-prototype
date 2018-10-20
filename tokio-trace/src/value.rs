0405ba0f125523e1ba43a474d45c6b8fdd5da413
Eliza Weisman <eliza@buoyant.io>
2018-10-20 14:36:08 -0700

    style cleanup & comments

--- tokio-trace/src/value.rs
+++ tokio-trace/src/value.rs
@@ -1,8 +1,10 @@
+//! Arbitrarily typed field values.
 use std::{any::Any, borrow::Borrow, fmt};
 
+/// A formattable field value of an erased type.
 pub trait Value: fmt::Debug + Send + Sync {}
 
-/// An owned, dynamically-typed value.
+/// An owned value of an erased type.
 ///
 /// Like `Any`, references to `OwnedValue` may attempt to downcast the value to
 /// a concrete type. However, unlike `Any`, `OwnedValue`s are constructed from
@@ -16,30 +18,36 @@ pub struct OwnedValue {
 
 impl<T> Value for T where T: fmt::Debug + Send + Sync {}
 
-// TODO: should `IntoValue` also be `Value`?
-pub trait IntoValue {
+/// Trait representing a value which may be converted into an `OwnedValue`.
+///
+/// References to types implementing `IntoValue` may be formatted (as `Value`s),
+/// _or_ may be converted into owned `OwnedValue`s. In addition to being owned,
+/// instances of `OwnedValue` may also be downcast to their original erased type.
+pub trait IntoValue: Value {
     // like `Clone`, but "different"
     fn into_value(&self) -> OwnedValue;
 }
 
 impl<T, V> IntoValue for T
 where
-    T: ToOwned<Owned = V>,
+    T: ToOwned<Owned = V> + Value,
     V: Borrow<T> + Value + 'static,
 {
     fn into_value(&self) -> OwnedValue {
         let any: Box<dyn Any + Send + Sync> = Box::new(self.to_owned());
-        debug_assert!(any.as_ref().downcast_ref::<V>().is_some());
-        // I'll just make the damn vtable *myself*!
+        debug_assert!(
+            any.as_ref().downcast_ref::<V>().is_some(),
+            "Box<Any> must be downcastable for OwnedValue to work",
+        );
+        // This closure "remembers" the original type `V` before it is erased, and
+        // can thus format the `Any` by downcasting it back to `V` and calling
+        // `V`'s debug impl.
         let my_debug_impl = |me: &Any, f: &mut fmt::Formatter| {
-            let me = me.downcast_ref::<V>()
-                .expect("type should downcast to its pre-erasure type");
-            fmt::Debug::fmt(me, f)
+            me.downcast_ref::<V>()
+                .expect("type should downcast to its pre-erasure type")
+                .fmt(f)
         };
-        OwnedValue {
-            my_debug_impl,
-            any,
-        }
+        OwnedValue { my_debug_impl, any }
     }
 }
 
