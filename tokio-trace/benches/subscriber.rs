de0190bca75f9b1aa57d7d992a6768c1b1488697
Eliza Weisman <eliza@buoyant.io>
2018-11-06 15:11:17 -0800

    actually add data in benchmarks

--- tokio-trace/benches/subscriber.rs
+++ tokio-trace/benches/subscriber.rs
@@ -5,6 +5,10 @@ extern crate test;
 use test::Bencher;
 
 use tokio_trace::{span, field, Meta, Event, subscriber};
+use std::{
+    collections::HashMap,
+    sync::Mutex,
+};
 
 /// A subscriber that is enabled but otherwise does nothing.
 struct EnabledSubscriber;
@@ -56,6 +60,60 @@ impl tokio_trace::Subscriber for EnabledSubscriber {
     }
 }
 
+/// Simulates a subscriber that caches span data.
+struct AddData(Mutex<Option<span::Data>>);
+
+
+impl tokio_trace::Subscriber for AddData {
+    fn new_span(&self, span: span::Data) -> span::Id {
+        *self.0.lock().unwrap() = Some(span);
+        span::Id::from_u64(0)
+    }
+
+    fn add_value(
+        &self,
+        span: &span::Id,
+        field: &field::Key,
+        value: &dyn field::IntoValue,
+    ) -> Result<(), tokio_trace::subscriber::AddValueError> {
+        let _ = span;
+        if let Some(data) = self.0.lock().unwrap().as_mut() {
+            data.add_value(field, value);
+        }
+        Ok(())
+    }
+
+    fn add_follows_from(&self, span: &span::Id, follows: span::Id) -> Result<(), subscriber::FollowsError> {
+        let _ = (span, follows);
+        Ok(())
+    }
+
+    fn enabled(&self, metadata: &Meta) -> bool {
+        let _ = metadata;
+        true
+    }
+
+    fn should_invalidate_filter(&self, _metadata: &Meta) -> bool {
+        false
+    }
+
+    fn observe_event<'event, 'meta: 'event>(&self, event: &'event Event<'event, 'meta>) {
+        let _ = event;
+    }
+
+    fn enter(&self, span: span::Id) {
+        let _ = span;
+    }
+
+    fn exit(&self, span: span::Id){
+        let _ = span;
+    }
+
+    fn close(&self, span: span::Id){
+        let _ = span;
+    }
+}
+
 #[bench]
 fn span_no_fields(b: &mut Bencher) {
     tokio_trace::Dispatch::to(EnabledSubscriber).as_default(|| {
@@ -68,6 +126,15 @@ fn span_no_fields(b: &mut Bencher) {
 #[bench]
 fn span_with_fields(b: &mut Bencher) {
     tokio_trace::Dispatch::to(EnabledSubscriber).as_default(|| {
+        b.iter(|| {
+            span!("span", foo = &"foo", bar = &"bar", baz = &3, quuux = &0.99)
+        })
+    });
+}
+
+#[bench]
+fn span_with_fields_add_data(b: &mut Bencher) {
+    tokio_trace::Dispatch::to(AddData(Mutex::new(None))).as_default(|| {
         b.iter(|| {
             span!("span", foo = &"foo", bar = &"bar", baz = &3, quuux = &0.99)
         })
