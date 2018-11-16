1227eb18b6c06ea7b123decb3272c7c359b5e0bb
Eliza Weisman <eliza@buoyant.io>
2018-11-16 12:34:00 -0800

    Add test that spans close after events within that span

--- tokio-trace/src/span.rs
+++ tokio-trace/src/span.rs
@@ -529,6 +529,63 @@ mod tests {
     }
 
     #[test]
+    fn span_closes_after_event() {
+        let subscriber = subscriber::mock()
+            .enter(span::mock().named(Some("foo")))
+            .event()
+            .exit(span::mock().named(Some("foo")))
+            .close(span::mock().named(Some("foo")))
+            .done()
+            .run();
+        Dispatch::new(subscriber).as_default(|| {
+            span!("foo").enter(|| {
+                Span::current().close();
+                event!(::Level::Debug, {}, "my event!");
+            });
+        })
+    }
+
+    #[test]
+    fn new_span_after_event() {
+        let subscriber = subscriber::mock()
+            .enter(span::mock().named(Some("foo")))
+            .event()
+            .exit(span::mock().named(Some("foo")))
+            .close(span::mock().named(Some("foo")))
+            .enter(span::mock().named(Some("bar")))
+            .exit(span::mock().named(Some("bar")))
+            .close(span::mock().named(Some("bar")))
+            .done()
+            .run();
+        Dispatch::new(subscriber).as_default(|| {
+            span!("foo").enter(|| {
+                Span::current().close();
+                event!(::Level::Debug, {}, "my event!");
+            });
+            span!("bar").enter(|| {
+                Span::current().close();
+            });
+        })
+    }
+
+    #[test]
+    fn event_outside_of_span() {
+        let subscriber = subscriber::mock()
+            .event()
+            .enter(span::mock().named(Some("foo")))
+            .exit(span::mock().named(Some("foo")))
+            .close(span::mock().named(Some("foo")))
+            .done()
+            .run();
+        Dispatch::new(subscriber).as_default(|| {
+            event!(::Level::Debug, {}, "my event!");
+            span!("foo").enter(|| {
+                Span::current().close();
+            });
+        })
+    }
+
+    #[test]
     fn dropping_a_span_calls_drop_span() {
         let subscriber = subscriber::mock()
             .enter(span::mock().named(Some("foo")))
