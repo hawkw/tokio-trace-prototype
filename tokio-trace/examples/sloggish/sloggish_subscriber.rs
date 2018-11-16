2abee8f052195a911a64ee45419f3538a5ff890e
Eliza Weisman <eliza@buoyant.io>
2018-11-16 13:53:02 -0800

    fix slog example

--- tokio-trace/examples/sloggish/sloggish_subscriber.rs
+++ tokio-trace/examples/sloggish/sloggish_subscriber.rs
@@ -14,7 +14,7 @@ extern crate ansi_term;
 extern crate humantime;
 use self::ansi_term::{Color, Style};
 use super::tokio_trace::{
-    self, field,
+    self,
     subscriber::{self, Subscriber},
     Id, Level, SpanAttributes,
 };
@@ -35,12 +35,21 @@ pub struct SloggishSubscriber {
     stderr: io::Stderr,
     stack: Mutex<Vec<Id>>,
     spans: Mutex<HashMap<Id, Span>>,
+    events: Mutex<HashMap<Id, Event>>,
     ids: AtomicUsize,
 }
 
 struct Span {
     attrs: SpanAttributes,
     kvs: Vec<(String, String)>,
+}
+
+struct Event {
+    id: Id,
+    level: tokio_trace::Level,
+    target: String,
+    message: String,
+    kvs: Vec<(String, String)>
 }
 
 struct ColorLevel(Level);
@@ -70,12 +79,42 @@ impl Span {
         key: &tokio_trace::field::Key,
         value: fmt::Arguments,
     ) -> Result<(), subscriber::RecordError> {
-        let mut s = String::new();
         // value.record(key, &mut tokio_trace::field::DebugRecorder::new(&mut s))?;
         // TODO: shouldn't have to alloc the key...
-        // self.kvs.push((key.name().unwrap_or("???").to_owned(), s));
-        unimplemented!()
-        // Ok(())
+        let k = key.name().unwrap_or("???").to_owned();
+        let v = fmt::format(value);
+        self.kvs.push((k, v));
+        Ok(())
+    }
+}
+
+impl Event {
+    fn new(attrs: tokio_trace::Attributes, id: Id) -> Self {
+        let meta = attrs.metadata();
+        Self {
+            id,
+            target: meta.target.to_owned(),
+            level: meta.level,
+            message: String::new(),
+            kvs: Vec::new(),
+        }
+    }
+
+    fn record(
+        &mut self,
+        key: &tokio_trace::field::Key,
+        value: fmt::Arguments,
+    ) -> Result<(), subscriber::RecordError> {
+        if key.name() == Some("message") {
+            self.message = fmt::format(value);
+            return Ok(())
+        }
+
+        // TODO: shouldn't have to alloc the key...
+        let k = key.name().unwrap_or("???").to_owned();
+        let v = fmt::format(value);
+        self.kvs.push((k, v));
+        Ok(())
     }
 }
 
@@ -86,6 +125,7 @@ impl SloggishSubscriber {
             stderr: io::stderr(),
             stack: Mutex::new(vec![]),
             spans: Mutex::new(HashMap::new()),
+            events: Mutex::new(HashMap::new()),
             ids: AtomicUsize::new(0),
         }
     }
@@ -117,15 +157,6 @@ impl SloggishSubscriber {
         Ok(())
     }
 
-    fn print_meta(&self, writer: &mut impl Write, meta: &tokio_trace::Meta) -> io::Result<()> {
-        write!(
-            writer,
-            "{level} {target} ",
-            level = ColorLevel(meta.level),
-            target = meta.target,
-        )
-    }
-
     fn print_indent(&self, writer: &mut impl Write, indent: usize) -> io::Result<()> {
         for _ in 0..(indent * self.indent_amount) {
             write!(writer, " ")?;
@@ -142,6 +173,10 @@ impl Subscriber for SloggishSubscriber {
     fn new_id(&self, span: tokio_trace::span::Attributes) -> tokio_trace::Id {
         let next = self.ids.fetch_add(1, Ordering::SeqCst) as u64;
         let id = tokio_trace::Id::from_u64(next);
+        self.events
+            .lock()
+            .unwrap()
+            .insert(id.clone(), Event::new(span, id.clone()));
         id
     }
 
@@ -161,13 +196,16 @@ impl Subscriber for SloggishSubscriber {
         name: &tokio_trace::field::Key,
         value: fmt::Arguments,
     ) -> Result<(), subscriber::RecordError> {
+        let mut events = self.events.lock().expect("mutex poisoned!");
+        if let Some(event) = events.get_mut(span) {
+            return event.record(name, value)
+        };
         let mut spans = self.spans.lock().expect("mutex poisoned!");
         let span = spans
             .get_mut(span)
             .ok_or_else(|| subscriber::RecordError::no_span(span.clone()))?;
-        // span.record(name, value)?;
-        unimplemented!()
-        // Ok(())
+        span.record(name, value)?;
+        Ok(())
     }
 
     fn add_follows_from(
@@ -215,7 +253,26 @@ impl Subscriber for SloggishSubscriber {
     fn exit(&self, _span: tokio_trace::Id) {}
 
     #[inline]
-    fn close(&self, _span: tokio_trace::Id) {
+    fn close(&self, id: tokio_trace::Id) {
+        if let Some(event) = self.events
+            .lock()
+            .expect("mutex poisoned")
+            .remove(&id)
+        {
+            let mut stderr = self.stderr.lock();
+            let indent = self.stack.lock().unwrap().len();
+            self.print_indent(&mut stderr, indent).unwrap();
+            write!(
+                &mut stderr,
+                "{timestamp} {level} {target} {message}",
+                timestamp = humantime::format_rfc3339_seconds(SystemTime::now()),
+                level = ColorLevel(event.level),
+                target = &event.target,
+                message = Style::new().bold().paint(event.message),
+            ).unwrap();
+            self.print_kvs(&mut stderr, event.kvs.iter().map(|&(ref k, ref v)| (k, v)), ", ").unwrap();
+            write!(&mut stderr, "\n").unwrap();
+        }
         // TODO: it's *probably* safe to remove the span from the cache
         // now...but that doesn't really matter for this example.
     }
