51db43a0717e37fce56c6496693073fa734acfd5
Eliza Weisman <eliza@buoyant.io>
2018-11-27 17:34:18 -0800

    fix macros

--- tokio-trace/src/lib.rs
+++ tokio-trace/src/lib.rs
@@ -4,38 +4,52 @@ extern crate tokio_trace_core;
 #[macro_export]
 macro_rules! callsite {
     (span: $name:expr, $( $field_name:ident ),*) => ({
-        callsite!(@ $crate::Meta {
+        callsite!(@
             name: Some($name),
             target: module_path!(),
             level: $crate::Level::TRACE,
-            module_path: Some(module_path!()),
-            file: Some(file!()),
-            line: Some(line!()),
-            field_names: &[ $(stringify!($field_name)),* ],
             kind: $crate::metadata::Kind::SPAN,
-        })
+            fields: &[ $(stringify!($field_name)),* ]
+        )
     });
     (event: $lvl:expr, $( $field_name:ident ),*) =>
         (callsite!(event: $lvl, target: module_path!(), $( $field_name ),* ));
     (event: $lvl:expr, target: $target:expr, $( $field_name:ident ),*) => ({
-        callsite!(@ $crate::Meta {
+        callsite!(@
             name: None,
+            target: $target,
+            level: $lvl,
+            kind: $crate::metadata::Kind::EVENT,
+            fields: &[ "message", $(stringify!($field_name)),* ]
+        )
+    });
+    (@
+        name: $name:expr,
+        target: $target:expr,
+        level: $lvl:expr,
+        kind: $kind:expr,
+        fields: $field_names:expr
+    ) => ({
+        use std::sync::{Once, atomic::{ATOMIC_USIZE_INIT, AtomicUsize, Ordering}};
+        use $crate::{callsite, Meta, subscriber::{Interest}, field::Fields};
+        struct MyCallsite;
+        static FIELDS: &'static [&'static str] = $field_names;
+        static META: Meta<'static> = $crate::Meta {
+            name: $name,
             target: $target,
             level: $lvl,
             module_path: Some(module_path!()),
             file: Some(file!()),
             line: Some(line!()),
-            field_names: &[ "message", $(stringify!($field_name)),* ],
-            kind: $crate::metadata::Kind::EVENT,
-        })
-    });
-    (@ $meta:expr ) => ({
-        use std::sync::{Once, atomic::{ATOMIC_USIZE_INIT, AtomicUsize, Ordering}};
-        use $crate::{callsite, Meta, subscriber::{Interest}};
-        static META: Meta<'static> = $meta;
+            fields: Fields {
+                names: FIELDS,
+                callsite: &MyCallsite,
+            },
+            callsite: &MyCallsite,
+            kind: $kind,
+        };
         static INTEREST: AtomicUsize = ATOMIC_USIZE_INIT;
         static REGISTRATION: Once = Once::new();
-        struct MyCallsite;
         impl MyCallsite {
             #[inline]
             fn interest(&self) -> Interest {
@@ -110,7 +124,7 @@ macro_rules! span {
             // not actually be used, but it doesn't make sense to repeat it.
             #[allow(unused_variables, unused_mut)]
             Span::new(callsite.interest(), callsite.metadata(), |span| {
-                let mut keys = callsite.metadata().fields();
+                let mut keys = callsite.metadata().fields().into_iter();
                 $(
                     let key = keys.next()
                         .expect(concat!("metadata should define a key for '", stringify!($k), "'"));
@@ -143,7 +157,7 @@ macro_rules! event {
             // not actually be used, but it doesn't make sense to repeat it.
             #[allow(unused_variables, unused_mut)]
             Event::new(callsite.interest(), callsite.metadata(), |event| {
-                let mut keys = callsite.metadata().fields();
+                let mut keys = callsite.metadata().fields().into_iter();
                 event.message(
                     &keys.next().expect("event metadata should define a key for the message"),
                     format_args!( $($arg)+ )
