6e7d0d550634703d6295d3726538ea6edd54cbb3
Eliza Weisman <eliza@buoyant.io>
2018-12-03 10:52:34 -0800

    Add docs for `metadata!`

--- tokio-trace-core/src/lib.rs
+++ tokio-trace-core/src/lib.rs
@@ -6,17 +6,24 @@ extern crate lazy_static;
 
 /// Statically constructs an [`Identifier`] for the provided [`Callsite`].
 ///
-/// This may be used in contexts, such as static initializers,  where the
-/// [`Callsite::id()`] function is not currently usable.
+/// This may be used in contexts, such as static initializers, where the
+/// [`Callsite::id`] function is not currently usable.
 ///
 /// [`Identifier`]: ::callsite::Identifier
 /// [`Callsite`]: ::callsite::Callsite
-/// [`Callsite::id()`]: ::callsite::Callsite::id
+/// [`Callsite::id`]: ::callsite::Callsite::id
 #[macro_export]
 macro_rules! identify_callsite {
     ($callsite:expr) => ($crate::callsite::Identifier($callsite))
 }
 
+/// Statically constructs a set of span [metadata].
+///
+/// This may be used in contexts, such as static initializers, where the
+/// [`Meta::new`] function is not currently usable.
+///
+/// [metadata]: ::metadata::Meta
+/// [`Meta::new`]: ::metadata::Meta::new
 #[macro_export]
 macro_rules! metadata {
     (
